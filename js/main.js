c7063dfc421b152d5ac2435a5c584eed40ad913c
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-10 14:19:39 -0400

    saving

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-// import axios from 'axios'
+import axios from 'axios'
 
 
-// function axiosSwanson () {
-//     axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes/1')
-//     .then(response => console.log('data: ', response.data))
-//     .catch(error => {
-//         console.log('error: ', error)
-//     })
-// }
+function axiosWeather () {
+    axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40504,US&appid=8d8a35e78ec3babb4db1abb8150ab7e4')
+
+    .then(response => console.log('data: ', response.data))
+    .catch(error => {
+        console.log('error: ', error)
+    })
+
+}
+
+axiosWeather()
 
 // function fetchSwanson(){
 // fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes/1')
@@ -19,3 +23,12 @@
 
 // }
 // fetchSwanson()
+
+
+// function fetchWeather(){
+    // fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={8d8a35e78ec3babb4db1abb8150ab7e4}')
+    // .then(response => response.json())
+    // .then(data => console.log('data: ', data))
+    // .catch(error => {
+    //     console.log('error: ', error)
+    // })
