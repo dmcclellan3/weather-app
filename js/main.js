15fae0b47bd3bb3c4fc999aece73fb1ac0ba756f
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-10 11:41:31 -0400

    saving

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// import axios from 'axios'
+
+
+// function axiosSwanson () {
+//     axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes/1')
+//     .then(response => console.log('data: ', response.data))
+//     .catch(error => {
+//         console.log('error: ', error)
+//     })
+// }
+
+// function fetchSwanson(){
+// fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes/1')
+// .then(response => response.json())
+// .then(data => console.log('data: ', data))
+// .catch(error => {
+//     console.log('error: ', error)
+// })
+
+// }
+// fetchSwanson()
