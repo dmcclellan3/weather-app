e8360e426e59a996c5a97a945afea697f5c0c20f
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-11 11:57:25 -0400

    updating

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
 
+const weatherBtn = document.getElementById("weather-button")
+const zipCodeInsert = document.getElementById("zip-code-insert")
+const cityField = document.getElementById("city-field")
+const kelvinField = document.getElementById("temperature-field-1")
+const celsiusField = document.getElementById("temperature-field-2")
+const fahrenheitField = document.getElementById("temperature-field-3")
+const conditionField = document.getElementById("condition-field")
+const infoField = document.getElementById("info-field")
 
-function axiosWeather () {
+
+weatherBtn.addEventListener("click", () => {
+
+getWeather()
+
+})
+
+function getWeather () {
     axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40504,US&appid=8d8a35e78ec3babb4db1abb8150ab7e4')
 
     .then(response => console.log('data: ', response.data))
@@ -11,7 +26,8 @@ function axiosWeather () {
 
 }
 
-axiosWeather()
+
+
 
 // function fetchSwanson(){
 // fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes/1')
